05640b4db0cfa25d8aa7406ec2d47e82d0d6ef88
Evan Ranjitkar <evanranjit7@gmail.com>
2025-02-14 22:37:51 -0600

    Finished work on DAOUtility

--- src/edu/jsu/mcis/cs310/coursedb/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/coursedb/dao/DAOUtility.java
@@ -11,13 +11,41 @@ public class DAOUtility {
     public static String getResultSetAsJson(ResultSet rs) {
         
         JsonArray records = new JsonArray();
+        ResultSetMetaData rsmd = null;
         
         try {
         
             if (rs != null) {
 
                 // INSERT YOUR CODE HERE
+                
+                // Getting metadata from ResultSet
+                rsmd = rs.getMetaData();
+                
+                // Getting the number of columns
+                int columnCount = rsmd.getColumnCount();
 
+                // While loop to loop through each  row 
+                while (rs.next()) {
+                    
+                    // Creating a JSON object for the current row
+                    JsonObject recordRow = new JsonObject();
+                    
+                    // for loop to loop through each column
+                    for (int i = 1; i <= columnCount; i++) {
+                        
+                        // Getting the column name
+                        String columnName = rsmd.getColumnName(i);
+                        
+                        // Getting the column value
+                        Object columnValue = rs.getObject(i);
+                        
+                        // Adding the column name and value in the row object
+                        recordRow.put(columnName, columnValue);
+                }
+                    // Adding the row object in the records array
+                    records.add(recordRow);
+                }
             }
             
         }
